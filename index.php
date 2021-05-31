5609a732266a9d375e224a28401433d35fecb8d4
Tim Hartsuijker <tim.hartsuijker@rocdeleijgraaf.nl>
2021-05-31 19:44:16 +0200

    1st push

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-
+<!--hier wordt het bord gemaakt-->
 <div class="bord">
     <?php
     require 'functions.php';
@@ -40,15 +40,17 @@
     }
     ?>
 </div>
+<!--hier worden de stenen geplaatst-->
 <div class="stenen">
     <?php
+    //zwarte stenen bovenaan
     $id= 1;
     for ($j = 1; $j <= 4; $j++) {
         if ($j % 2 == 0) {
             for ($kolom = 1; $kolom <= 5; $kolom++) {
                 for ($rows = 1; $rows <= 1; $rows++) {
                     ?>
-                    <div class="steenZwart" id="<?=$id?>"></div>
+                    <a href="index.php?id=<?= $id?>" class="steenZwart" id="<?=$id?>"></a>
                     <div class="steenLeeg"></div>
                     <?php
                     $id++;
@@ -62,7 +64,7 @@
                 for ($rows = 1; $rows <= 1; $rows++) {
                     ?>
                     <div class="steenLeeg"></div>
-                    <div class="steenZwart" id="<?=$id?>"></div>
+                    <a href="index.php?id=<?= $id?>" class="steenZwart" id="<?=$id?>"></a>
                     <?php
                     $id++;
                 }
@@ -72,6 +74,7 @@
             <?php
         }
     }
+    //lege vakjes in het midden
     for ($i = 1; $i <= 2; $i++) {
         for ($kolom = 1; $kolom <= 5; $kolom++) {
             for ($rows = 1; $rows <= 2; $rows++) {
@@ -84,14 +87,13 @@
         <br>
         <?php
     }
-    ?>
-    <?php
+  //witte stenen onderaan
     for ($j = 1; $j <= 4; $j++) {
         if ($j % 2 == 0) {
             for ($kolom = 1; $kolom <= 5; $kolom++) {
                 for ($rows = 1; $rows <= 1; $rows++) {
                     ?>
-                    <div class="steenWit"  id="<?=$id?>"></div>
+                    <a href="index.php?id=<?= $id?>" class="steenWit"  id="<?=$id?>"></a>
                     <div class="steenLeeg"></div>
                     <?php
                     $id++;
@@ -105,7 +107,7 @@
                 for ($rows = 1; $rows <= 1; $rows++) {
                     ?>
                     <div class="steenLeeg"></div>
-                    <div class="steenWit"  id="<?=$id?>"></div>
+                    <a href="index.php?id=<?= $id?>" class="steenWit" id="<?=$id?>"></a>
                     <?php
                     $id++;
                 }
@@ -117,5 +119,13 @@
     }
     ?>
 </div>
+<?php
+//laat het id van de aangeklikte steen zien
+if(isset($_GET['id'])){
+    echo "het id van de aangeklikte steen is " . $_GET['id'];
+}
+
+
+?>
 </body>
 </html>
